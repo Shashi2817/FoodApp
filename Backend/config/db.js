aba0dc68a78d4e6ae9068457527766473e38a1a0
Shashi2817 <shashikumar2817@gmail.com>
2024-08-01 10:39:09 +0530

    commit

---
+++ Backend/config/db.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+import 'dotenv/config'
+
+export const connectdb = async ()=>{
+    // await mongoose.connect('mongodb://localhost:27017/food-del').then(console.log('Database connected'))
+    await mongoose.connect(process.env.MONGO_URI||'mongodb://localhost:27017/food-del').then(console.log('Database connected'))
+}
---
+++ Backend/server.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import cors from 'cors'
+import { connectdb } from './config/db.js';
+import foodRouter from './routes/foodRouts.js';
+import userRouter from './routes/userRouts.js';
+import 'dotenv/config'
+import cartRouter from './routes/cartRouts.js';
+import orderRouter from './routes/orderRouts.js';
+import orderModel from './models/orderModel.js';
+//app config
+const app = express();
+const port = process.env.PORT ||4000;
+
+
+
+//middleware
+
+app.use(express.json())
+app.use(cors())
+
+//database connection
+
+connectdb();
+
+
+//app end points
+app.use("/api/food", foodRouter)
+app.use('/images', express.static('uploads'))
+app.use('/api/user', userRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/order', orderRouter)
+
+// app.get('/',(req, res)=>{
+//     res.send('API is working')
+// })
+
+
+app.listen(port,()=>{
+    console.log(`server is running on http://localhost:${port} `)
+})
